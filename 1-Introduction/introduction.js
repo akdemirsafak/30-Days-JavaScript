d7f582db019308a152b8230369884d66f50128d0
akdemirsafak <akdemirsafak@gmail.com>
2024-05-12 16:34:04 +0300

    Introduction

---
+++ 1-Introduction/introduction.js
@@ -0,0 +1,28 @@
+let firstName
+console.log(firstName) //Undefined - because it is not assigned a value yet.
+
+let secondName=null
+console.log(secondName)
+
+let number1=1;
+
+//Check data types
+console.log(typeof 'Text')
+
+console.log(typeof 6)
+
+console.log(typeof true)
+
+console.log(typeof number1)
+
+
+//Variables 
+//Variables are used to store data in a memory location
+//To declare a variable, we use var,let or const keywords.
+// For a variable that changes at a different time, we use let.If the data doesn't change at all, we use const.
+
+//A JavaScript name should not begin with a number
+//A JavaScript name doesnt allow special characters except $ and _ 
+//A JavaScript name is case sensitive
+
+let name="Adam", lastName="John", Age=3;
