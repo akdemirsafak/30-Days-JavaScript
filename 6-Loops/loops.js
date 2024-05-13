90921950e79bec1b71bf601d7360850d87f49bc3
akdemirsafak <akdemirsafak@gmail.com>
2024-05-13 11:53:34 +0300

    loops

---
+++ 6-Loops/loops.js
@@ -0,0 +1,46 @@
+let number = 12
+// For loop
+// console.log("For")
+// for (let i = 0; i < number; i++) {
+//     console.log(i)
+// }
+
+// //while 
+// console.log("While")
+// while(number >0){
+//     console.log(number)
+//     number--
+// }
+// do while
+
+
+// let persons=["Şafak","Adam","John"]
+
+// console.log("\n-------forEach-------\n\n")
+// persons.forEach((person)=>console.log(person))
+
+// console.log("\n-------For in-------\n\n")
+// for(let x in persons)
+// {
+//     console.log(persons[x])
+// }
+
+//for of
+//  console.log("\n-------ForOf-------\n\n")
+//  const cars = ["BMW", "Volvo", "Mini"];
+
+// for (let x of cars) {
+//   console.log(x)
+// }
+
+
+/// ITERABLES
+
+let letters=['a','b','c','d','e']
+
+for(const x of letters){
+    console.log(x)
+}
+
+
+
