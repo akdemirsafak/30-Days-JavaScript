b7af4c18f66d0266a03a9a45a169d54ff1f15580
akdemirsafak <akdemirsafak@gmail.com>
2024-05-24 12:14:37 +0300

    chore: Add DOM manipulation examples and explanations

---
+++ 22-Manupilating_Dom/index.js
@@ -0,0 +1,49 @@
+// ---------- Manupilating Dom Objects
+
+
+
+// ? -------- Create an Element
+
+// let subtitle= document.createElement('h3')
+// subtitle.textContent = 'This is a subtitle'
+// subtitle.classList.add('subtitle')
+// subtitle.setAttribute('id', 'subtitle')
+// subtitle.style.backgroundColor='red'
+
+// console.log(subtitle)
+//document.body.appendChild(subtitle)
+
+
+// ? -------- Create Multiple Elements
+
+  // let title
+  //       for (let i = 0; i < 3; i++) {
+  //           title = document.createElement('h1')
+  //           title.className = 'title'
+  //           title.style.fontSize = '24px'
+  //           title.textContent = i
+  //           console.log(title)
+  //       }
+
+  // ? ------ Appending Child to a Parent Element
+
+  // let title
+  // for(let i=0;i<3;i++){
+  //   title = document.createElement('h1')
+  //   title.className = 'title'
+  //   title.style.fontSize = '18px'
+  //   title.textContent = i
+  //   document.body.appendChild(title)
+  // }
+
+  // ? ------ Removing a Child Element From a Parent Node
+
+  // const ul= document.querySelector('ul')
+  // const lists=document.querySelectorAll('li')
+  // lists.forEach(list=>{
+  //   ul.removeChild(list)
+  // })
+
+
+  // const ul= document.querySelector('ul')
+  // ul.innerHTML=''
